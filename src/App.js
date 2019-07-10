94f8da5fe28451d8e1dec3eab38d4fa592f47f23
Aaron McPherson <aaronmcpherson@hotmail.com>
2019-07-10 14:34:10 +0700

    first

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css'
 import XtermPage from './components/XtermPage'
 
 const Index = () => {
-
+  const url = 'http://localhost:3000/xterm'
   return (
     <div>
-      <h1>Try your luck</h1>
+      <h1>CLick button to open window or go to:</h1>
+      <h1><a href={url}>Go to xterm</a></h1>
 
       <button onClick={() => {
-        const url = 'http://localhost:3000/xterm'
+
         const iframe = `<html><head><style>body, html {width: 100%; height: 100%; margin: 0; padding: 0}</style></head><body><iframe src='${url}' style='height:calc(100% - 4px);width:calc(100% - 4px)'></iframe></html></body>`;
         const windowConfig = 'menubar=no,location=no,resizable=no,scrollbars=no,status=yes,width=400,height=350'
 
